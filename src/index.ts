908ff8771e640c14b104e28ff44dd21e10508c58
Antonio <jr_acn@yahoo.com.br>
2022-06-25 02:55:40 -0300

    feat: adjusts

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,9 @@
 import { loadEnvs } from './tools/envs.tools';
 const env = process.env.NODE_ENV || 'development';
 loadEnvs(`${__dirname}/../envs/.env-${env}`);
+import  './mysql';
 import './queues';
-import AppDataSource from './mysql';
 import './server';
-
-AppDataSource.initialize()
-  .then(() => {
-    console.log('MYSQL conectado com sucesso :)');
-  })
-  .catch((error) => {
-    console.error('Falha ao conectar ao MYSQL');
-    console.error(error);
-  });
 
 process.on('uncaughtException', (error: Error) => {
   console.error(`uncaughtException ${error.message}`);
--- src/mysql/index.ts
+++ src/mysql/index.ts
@@ -12,4 +12,13 @@ const AppDataSource = new DataSource({
   subscribers: [],
 });
 
+AppDataSource.initialize()
+  .then(() => {
+    console.log('MYSQL conectado com sucesso :)');
+  })
+  .catch((error) => {
+    console.error('Falha ao conectar ao MYSQL');
+    console.error(error);
+  });
+
 export default AppDataSource;
